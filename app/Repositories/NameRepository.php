48bc7cf93fd1c33dc851750522c0536e5c709ae1
AmyKagerou <kagerouayano2291@gmail.com>
2026-05-05 15:46:58 +0700

    first commit

---
+++ app/Repositories/NameRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+class NameRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
